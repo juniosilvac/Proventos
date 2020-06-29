462321068be58dfa20d6ccde8e2399609b7f6dcb
Junio Silva <junio.silvac@gmail.com>
2020-06-28 23:00:30 -0300

    criado o projeto API

---
+++ src/Proventos.Api/Controllers/ProventosController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Proventos.Core.Dtos;
+using Proventos.Core.Dtos.Requests;
+
+namespace Proventos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProventosController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProventosController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<ProventoDto>>> Get()
+        {
+            var obterProventosRequest = await _mediator.Send(new ObterProventosRequest());
+
+            if (!obterProventosRequest.HasError)
+            {
+                return Ok(obterProventosRequest.Data);
+            }
+            else
+            {
+                return BadRequest(obterProventosRequest.Errors);
+            }
+        }
+    }
+}
